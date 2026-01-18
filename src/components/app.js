9267f4a927011e1274ce5146c0a5b19cb6cc4196
Rachel Fingerhut <fingerhutrachel@gmail.com>
2026-01-18 14:56:58 -0800

    feat: add functions to impact todos and projects

--- src/components/app.js
+++ src/components/app.js
@@ -1,37 +1,104 @@
-import createProject from "./project";
-import createTodo from "./todo";
+import Project from "./project";
+import Todo from "./todo";
 
 function createApp() {
    const projects = [];
    let activeProject = null;
 
    function addProject(title){
-      const project = createProject(title);
+      const project = new Project(title);
       projects.push(project);
       activeProject = project;
+      return project;
    }
 
    function getActiveProject(){
       return activeProject;
    }
 
+   function setActiveProject(project){
+      activeProject = project;
+   }
+
    function getProjectById(id){
-      // find project with matching id
       const project = projects.find((el) => el.id === id);
       return project;
    }
 
-   function addTodo(title, projectID){
-      const todo = createTodo(title, 'desc', 'pri', 'date');
-      // select matching project and then push this todo onto the list of todos.
-      const project = getProjectById(projectID);
-      project.todos.push(todo);
+   function addTodo(title, desc, pri, date){
+      const project = getActiveProject();
+      if (!project) return false;
+
+      const todo = new Todo(title, desc, pri, date);
+      project.addTodoToList(todo);
+      return true;
    }
+
+   function getAllProjects(){
+      return projects;
+   }
+
+   function deleteActiveProject(){
+     const index = projects.findIndex((el) => el.id === activeProject.id);
+
+      if (index === -1){
+         return false;
+      }
+
+      projects.splice(index, 1);
+      activeProject = null;
+      return true; 
+   }
+
+   function deleteProjectByID(projectID){
+      const index = projects.findIndex((el) => el.id === projectID);
+
+      if (index === -1){
+         return false;
+      }
+
+      projects.splice(index, 1);
+      return true;
+   }
+
+   function showAllTodosOfActiveProject(){
+      const project = getActiveProject();
+      return project.getAllTodos();
+   }
+
+   function deleteTodo(id){
+      const project = getActiveProject();
+      if(!project){
+         return false;
+      }
+
+      return project.removeTodoByID(id);      
+   }
+
+   function markTodoComplete(id){
+      const project = getActiveProject();
+      const todo = project.getAllTodos().find((el) => el.id === id)
+      
+      if (!todo){
+         return false;
+      }
+
+      todo.markComplete();
+      return true;
+   }
+
 
    return {
       addProject,
       getActiveProject,
       addTodo,
+      getAllProjects,
+      deleteActiveProject,
+      deleteProjectByID,
+      showAllTodosOfActiveProject,
+      setActiveProject,
+      deleteTodo,
+      markTodoComplete,
    };
 }
 
