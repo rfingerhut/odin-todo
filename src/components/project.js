040fda5b94019b458903a746fd020902db1962a7
Rachel Fingerhut <fingerhutrachel@gmail.com>
2026-01-18 14:56:09 -0800

    feat: add functionality

--- src/components/project.js
+++ src/components/project.js
@@ -4,6 +4,29 @@ class Project{
         this.todos = [];
         this.id = crypto.randomUUID();
     }
+
+    setTitle(newTitle){
+        this.title = newTitle;
+    }
+
+    addTodoToList(todo){
+        this.todos.push(todo);
+    }
+
+    getAllTodos(){
+        return this.todos;
+    }
+
+    removeTodoByID(id){
+        const index = this.todos.findIndex((el) => el.id === id);
+
+        if (index === -1){
+            return false;
+        }
+
+        this.todos.splice(index, 1);
+        return true;
+    }
 }
 
 export default Project;
