c44df088212ee9f9511acc480704107d46094283
Rachel Fingerhut <fingerhutrachel@gmail.com>
2026-01-20 19:55:34 -0800

    chore: remove testing code lines

--- src/index.js
+++ src/index.js
@@ -1,22 +1,7 @@
 import "./style.css";
-import createApp from './components/app';
+import createApp from "./components/app";
+import ui from "./components/ui";
 
-function runTests() {
-  const app = createApp();
+const app = createApp();
 
-  console.log("=== START TESTS ===");
-
-  // 1. Test addProject
-  const projectA = app.addProject("Project A");
-  console.log("Project C created:", projectA);
-  console.log("Active project:", app.getActiveProject());
-    const projectC = app.addProject("Project C");
-
-
-  // 5. Test addTodo (should add to Project A)
-   app.addTodo("Todo 1", "desc", "high", "2026-01-01");
-   app.addTodo("Todo 2", "desc", "low", "2026-01-02");
-   app.addTodo("Todo 3", "desc", "low", "2026-01-02");
-}
-
-runTests();
+// ui.init(app);
